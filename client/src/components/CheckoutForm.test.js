b85f5854700a7bf52004e54edd19c04dae1ca142
Mark Goldhardt <markgoldhardt@Marks-iMac.hsd1.ut.comcast.net>
2020-05-18 18:57:41 -0600

    mvp

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -1,9 +1,17 @@
 import React from "react";
+import App from '../App';
+import useForm from '../hooks/useForm';
 import { render } from "@testing-library/react";
-import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
 
-test("form header renders", () => {});
+test("form header renders",  () => {
+    const { getByText } = render (<App/>);
+    const header = getByText(/React Plants/i);
+    expect(header).toBeInTheDocument();
+})
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", async () => {
+    const { getByText } = render (<useForm/>);
+    const message = getByText(/You have ordered some plants/i);
+})
